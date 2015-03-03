04cffe0886faf43ae0298fd94e3a2628993748fc
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2015-03-03 09:43:40 +0100

    Omnavngiv service.

--- app/src/main/java/dk/aau/cs/psylog/psylog_soundmodule/Service.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_soundmodule/Service.java
@@ -2,7 +2,7 @@ package dk.aau.cs.psylog.psylog_soundmodule;
 
 import dk.aau.cs.psylog.module_lib.SuperService;
 
-public class SoundService extends SuperService {
+public class Service extends SuperService {
     @Override
     public void setSensor() {
         sensor = new SoundMeter();
