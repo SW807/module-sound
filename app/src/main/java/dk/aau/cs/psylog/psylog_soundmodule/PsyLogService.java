b4c4691b2b95da11c6cd45e033653aff41fe4da5
Mathias Winde <Mapede11@student.aau.dk>
2015-03-03 11:10:57 +0100

    Ændrede Service til PsyLogService

--- app/src/main/java/dk/aau/cs/psylog/psylog_soundmodule/PsyLogService.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_soundmodule/PsyLogService.java
@@ -2,7 +2,7 @@ package dk.aau.cs.psylog.psylog_soundmodule;
 
 import dk.aau.cs.psylog.module_lib.SuperService;
 
-public class Service extends SuperService {
+public class PsyLogService extends SuperService {
     @Override
     public void setSensor() {
         sensor = new SoundMeter();
