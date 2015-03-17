50f0d9ee1dab46a9d6dac2a71cff64e065ee4c1a
Mathias Winde <Mapede11@student.aau.dk>
2015-03-17 12:50:19 +0100

    Ændrede servicen til at benytte SensorService

--- app/src/main/java/dk/aau/cs/psylog/sound/PsyLogService.java
+++ app/src/main/java/dk/aau/cs/psylog/sound/PsyLogService.java
@@ -1,8 +1,8 @@
 package dk.aau.cs.psylog.sound;
 
-import dk.aau.cs.psylog.module_lib.SuperService;
+import dk.aau.cs.psylog.module_lib.SensorService;
 
-public class PsyLogService extends SuperService {
+public class PsyLogService extends SensorService {
     @Override
     public void setSensor() {
         sensor = new SoundMeter(this);
